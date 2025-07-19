c382da8e041303d7d33a65b47b3aa61f5e2b0e6d
Assهm Khalid <assimkhalidali@gmail.com>
2025-07-20 02:22:13 +0300

    First commit

---
+++ src/components/ui/Header.jsx
@@ -0,0 +1,17 @@
+import searchIcon from "../../assets/search-engine.png";
+import Logo from "./Logo";
+import NabBar from "./NavBar";
+
+const Header = () => (
+  <header className="header">
+    <Logo />
+    
+    <NabBar />    
+
+    <div className="header_search">
+      <img src={searchIcon} alt="searchIcon" />
+    </div>
+  </header>
+);
+
+export default Header;
---
+++ src/components/ui/Logo.jsx
@@ -0,0 +1,11 @@
+import logo from "../../assets/logo.png";
+
+const Logo = () => (
+  <div className="header_logo">
+    <a href="/">
+      <img src={logo} alt="Logo" />
+    </a>
+  </div>
+);
+
+export default Logo;
